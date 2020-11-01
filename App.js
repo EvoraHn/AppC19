30ebddc8002e34d5fb3980f0262e800786094852
Ariel Lutz <ariellutz10@gmail.com>
2020-11-01 12:30:25 -0600

    Creacion del proyecto expo CovidApp

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import MenuCovidScreen from "./src/screens/MenuCovidScreen"; //la funcionalidad de este archivo la usamos aqui
+
+
+
+export default function App() {
+  return <MenuCovidScreen></MenuCovidScreen>;
+}
